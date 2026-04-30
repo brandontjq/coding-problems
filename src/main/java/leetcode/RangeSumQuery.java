94a6c3bd6e9adf157adf55afee32a03c265b5d0d
Brandon <brandontjq@gmail.com>
2026-04-30 12:01:09 +0800

    Range sum Query

---
+++ src/main/java/leetcode/RangeSumQuery.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class RangeSumQuery {
+    private int[] prefixArr;
+
+    public RangeSumQuery(int[] nums) {
+        prefixArr = new int[nums.length];
+        int prefixSum = nums[0];
+        prefixArr[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixSum += nums[i];
+            prefixArr[i] = prefixSum;
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return prefixArr[right];
+        }
+        return prefixArr[right] - prefixArr[left - 1];
+    }
+}
